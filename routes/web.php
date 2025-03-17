c45a510d28ae1a4427ba29cde42f0cca1acf495a
Nazmul Hossain Rafi <nazmulhossainrafi33@gmail.com>
2025-03-18 00:24:03 +0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,23 @@
+<?php
 
+
+
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
+
+// Home page route
+Route::get('/', [SiteController::class, 'homePage'])->name('home');
+
+// About page route
+Route::get('/about', [SiteController::class, 'aboutPage'])->name('about');
+
+// Services page route
+Route::get('/services', [SiteController::class, 'servicesPage'])->name('services');
+
+// Portfolio page route
+Route::get('/portfolio', [SiteController::class, 'portfolioPage'])->name('portfolio');
+
+// Contact page route
+Route::get('/contact', [SiteController::class, 'contactPage'])->name('contact');
